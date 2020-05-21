358c57f2b3e39164eb9acc810be0a0a3ef4fe96e
Yee, Richard <richard_yee@intuit.com>
2020-05-20 22:43:54 -0700

    removed unnecessary files, App.js changes to display main page

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -6,7 +6,7 @@ import {
 
 import { PageLayout } from './blocks/PageLayout';
 import { PageHeader } from './PageHeader';
-// import { MainMenu } from './MainMenu';
+import { MainMenu } from './MainMenu';
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
 
@@ -15,12 +15,31 @@ import { Sidebar } from './blocks/Sidebar';
 import { PageFooter } from './PageFooter';
 import { Content } from './blocks/Content';
 import { Provider } from 'react-redux';
-// import { carToolStore } from '../store/carToolStore';
-// import { colorToolStore } from '../store/colorToolStore';
-// import { calcStore } from '../store/calcStore';
 
 export const App = () => {
     return (
-       <div>dsfdfds</div>
+        <PageLayout>
+            <PageHeader />
+            <MainMenu />
+            <Content>
+                <Switch>
+                    <Route path="/" exact>
+                        <HomePage />
+                    </Route>
+                    <Route path="/about" exact>
+                        <AboutPage />
+                    </Route>
+                </Switch>
+            </Content>
+            <Switch>
+                <Route path="/about" exact>
+                    <Sidebar className="about-page" />
+                </Route>
+                <Route path="/" >
+                    <Sidebar className="home-page" />
+                </Route>
+            </Switch>
+            <PageFooter />
+        </PageLayout>
     );
 };
---
+++ app/src/components/MainMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './MainMenu.css';  
+
+export const MainMenu = () => {
+
+  // ui model data
+  const menuItems = [
+    { id: 1, url: '/', caption: 'Home' },
+    { id: 2, url: '/about', caption: 'About' },
+  ];
+
+  return (
+    <nav id="main-menu">
+      <ul>
+        {menuItems.map(menuItem => <li key={menuItem.id}>
+          <Link to={menuItem.url}>{menuItem.caption}</Link>
+        </li>)}
+      </ul>
+    </nav>
+  );
+
+};
