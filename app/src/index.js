e7bf1fc4b1cd20d34f632c390fbe0b9f3e530856
Shalini Grassi <Shalini_Grassi@intuit.com>
2020-05-20 21:45:16 -0700

    Added changes

--- app/src/index.js
+++ app/src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
-import { ApolloProvider } from 'react-apollo';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloClient } from 'apollo-client';
-
-import './index.css';
-
-const GRAPHQL_PORT = process.env.REACT_APP_GRAPHQL_PORT || 3010;
-
-const cache = new InMemoryCache();
-
-const link = new HttpLink({
-  uri: `http://localhost:${GRAPHQL_PORT}/graphql`,
-});
-
-const client = new ApolloClient({
-  link, cache,
-  connectToDevTools: true,
-});
+import { App } from './components/App';
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <Router>
     <App />
-  </ApolloProvider>,
-  document.getElementById('root'),
+  </Router>,
+  document.querySelector('#root'),
 );
+
