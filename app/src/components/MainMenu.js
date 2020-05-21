cbf9e3444f31bf5fd18d2bff5848cd89ccd65313
Yee, Richard <richard_yee@intuit.com>
2020-05-20 22:51:29 -0700

    update app name and menu items

--- app/src/components/MainMenu.js
+++ app/src/components/MainMenu.js
@@ -7,8 +7,9 @@ export const MainMenu = () => {
 
   // ui model data
   const menuItems = [
-    { id: 1, url: '/', caption: 'Home' },
-    { id: 2, url: '/about', caption: 'About' },
+    { id: 1, url: '/voter_reg', caption: 'Voter Registration' },
+    { id: 2, url: '/election_mgmt', caption: 'Election Management' },
+    { id: 3, url: '/voting', caption: 'Voting'}
   ];
 
   return (
--- app/src/components/PageHeader.js
+++ app/src/components/PageHeader.js
@@ -6,7 +6,7 @@ export const PageHeader = () => {
     return (
         <header id="page-header">
             <div className="header">
-                <h1>Cool Company, Inc.</h1>
+                <h1>Grumpy Cat Electronic Voting System</h1>
             </div>
         </header>
         );
