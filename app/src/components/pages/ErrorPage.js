f748d9ccce6b8381bde1ece85e01fc566985f5c4
Shalini Grassi <Shalini_Grassi@intuit.com>
2020-05-21 20:33:42 -0700

    Added changes for all

--- app/src/components/pages/ErrorPage.js
+++ app/src/components/pages/ErrorPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+import './ErrorPage.css';
 
 export const ErrorPage = (props) => {
     const {message} = props;
 
     return (
-        <div>{message}</div>
+        <div class="error-msg">{message}</div>
     );
 }
